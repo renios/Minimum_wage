5f5bfafd995938999cd821ddc003be42c8f19b89
Minjung Kim <Reny.atkins@gmail.com>
2017-12-29 04:41:39 +0900

    손님 주문/타이머 구현

---
+++ Assets/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Customer : MonoBehaviour {
+
+	public float waitingTime;
+	float remainWaitingTime;
+	public Image timerImage;
+
+	List<GameObject> orderedFoods;
+
+	void InitializeTimer() {
+		remainWaitingTime = waitingTime;
+		timerImage.fillAmount = remainWaitingTime / waitingTime;
+	}
+
+	void UpdateTimer() {
+		remainWaitingTime -= Time.deltaTime;
+		timerImage.fillAmount = remainWaitingTime / waitingTime;
+	}
+
+	void MakeOrder() {
+		orderedFoods = new List<GameObject>();
+		GetComponentsInChildren<FoodInOrder>().ToList().ForEach(fio => orderedFoods.Add(fio.gameObject));
+	}
+
+	// Use this for initialization
+	void Start () {
+		MakeOrder();
+		InitializeTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateTimer();
+	}
+}
---
+++ Assets/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CustomerManager : MonoBehaviour {
+
+	public GameObject customerPrefab;
+	public List<Transform> customerSlot;
+
+	// Use this for initialization
+	void Start () {
+		customerSlot.ForEach(t => {
+			GameObject customer = Instantiate(customerPrefab, t.position, Quaternion.identity);
+			customer.transform.parent = this.transform;
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
