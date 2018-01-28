0d2c1f374b13e005d160a7570d1a660f293e257d
Minjung Kim <Reny.atkins@gmail.com>
2018-01-28 14:16:54 +0900

    인게임 UI 추가

---
+++ Assets/MenuButtonInStage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonInStage : MonoBehaviour {
+
+	public void BackToWorld() {
+		SceneManager.LoadScene("World");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
