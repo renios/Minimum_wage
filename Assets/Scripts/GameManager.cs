61b2bd07fb278b1a698de274fb8f343690f2eaba
Minjung Kim <Reny.atkins@gmail.com>
2018-01-28 17:36:25 +0900

    인게임 레이아웃 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,17 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
 	public GameObject gameoverCanvas;
+	public Text textInCanvas;
 
 	public void ShowGameoverCanvas() {
 		gameoverCanvas.SetActive(true);
+		textInCanvas.text = "Game Over" + '\n' + '\n' + "Touch to Restart";
+		Time.timeScale = 0;
+	}
+
+	public void ShowClearCanvas() {
+		gameoverCanvas.SetActive(true);
+		textInCanvas.text = "Mission Clear" + '\n' + '\n' + "Touch to Restart";
 		Time.timeScale = 0;
 	}
 
@@ -22,7 +30,7 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (gameoverCanvas.activeInHierarchy && Input.anyKeyDown) {
-			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene("World");
 		}
 	}
 }
