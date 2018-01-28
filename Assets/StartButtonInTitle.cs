7001140826c8f1363c769b9231fb7217c8ecf87d
Minjung Kim <Reny.atkins@gmail.com>
2018-01-28 12:56:04 +0900

    타이틀->스테이지 플로우 구현

---
+++ Assets/StartButtonInTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonInTitle : MonoBehaviour {
+
+	public void GoToWorldScene() {
+		SceneManager.LoadScene("World");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
