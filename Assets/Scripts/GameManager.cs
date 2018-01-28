c2083c3c4709d50faddaf7e038147e9a648df12a
Minjung Kim <Reny.atkins@gmail.com>
2018-01-28 18:52:14 +0900

    상단부 배경 적용

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour {
 	public GameObject gameoverCanvas;
 	public Text textInCanvas;
 
+	bool isEnd;
+
 	public void ShowGameoverCanvas() {
 		gameoverCanvas.SetActive(true);
 		textInCanvas.text = "Game Over" + '\n' + '\n' + "Touch to Restart";
@@ -21,16 +23,29 @@ public class GameManager : MonoBehaviour {
 		Time.timeScale = 0;
 	}
 
+	void ShowMissionStartCanvas() {
+		Time.timeScale = 0;
+		gameoverCanvas.SetActive(true);
+		textInCanvas.text = "Mission Start!" + '\n' + '\n' + "Touch the Screen";
+	}
+
 	// Use this for initialization
 	void Start () {
+		isEnd = false;
 		Time.timeScale = 1;
-		gameoverCanvas.SetActive(false);
+		ShowMissionStartCanvas();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (gameoverCanvas.activeInHierarchy && Input.anyKeyDown) {
-			SceneManager.LoadScene("World");
+			if (isEnd)
+				SceneManager.LoadScene("World");
+			else {
+				gameoverCanvas.SetActive(false);
+				Time.timeScale = 1;
+				isEnd = true;
+			}
 		}
 	}
 }
