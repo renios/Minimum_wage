fa8275b6448734c7beb24e0f6091474875a51c45
youngsang <elbry92@gmail.com>
2018-02-13 16:49:01 +0900

    UI 버튼 피드백 애니메이션 구현

--- Assets/MenuButtonInStage.cs
+++ Assets/MenuButtonInStage.cs
@@ -2,20 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtonInStage : MonoBehaviour {
+    public bool  isHeld;
+    public float feedbackTime;
+    public float maxFeedbackTime;
+    public float feedbackRate;
+    public float settleRate;
+    Vector2 originalScale;
+    Image buttonImage;
 
-	public void BackToWorld() {
-		SceneManager.LoadScene("World");
-	}
+    public void StartFeedback()
+    {
+        if(isHeld==false)
+        {
+            isHeld = true;
+            originalScale = buttonImage.transform.localScale;
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
+    public void EndFeedback()
+    {
+        isHeld = false;
+        feedbackTime = 0;
+        buttonImage.transform.localScale = originalScale;
+        SceneManager.LoadScene("World");
+    }
+
+    // Use this for initialization
+    void Start () {
+        isHeld = false;
+        buttonImage = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isHeld==true)
+        {
+            print("is held");
+            feedbackTime += Time.deltaTime;
+            if(feedbackTime<maxFeedbackTime)
+            {
+                buttonImage.transform.localScale = originalScale * (1f + feedbackRate * (feedbackTime / maxFeedbackTime));
+            }
+            else
+            {
+                buttonImage.transform.localScale = originalScale * (1f + settleRate);
+            }
+        }
 	}
 }
