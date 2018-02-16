6104079aebdf007ea01680286b232ae0f7dc6665
Minjung Kim <Reny.atkins@gmail.com>
2018-02-16 21:12:05 +0900

    테두리 쉐이더 만듦

---
+++ Assets/Scripts/HighlightBorder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HighlightBorder : MonoBehaviour {
+
+	public SpriteRenderer spriteRenderer;
+	public Material highlightBorderMaterial;
+	Material defaultMaterial;
+
+	Coroutine playingCoroutine;
+
+	readonly float delay = 1.0f;
+
+	public void ActiveBorder() {
+		spriteRenderer.material = highlightBorderMaterial;
+		Coroutine coroutine = StartCoroutine(ChangeColor());
+	}
+
+	public void InactiveBorder() {
+		StopAllCoroutines();
+		spriteRenderer.material = defaultMaterial;
+	}
+
+	IEnumerator ChangeColor() {
+		Tween tw;
+		while (true) {
+			spriteRenderer.material.color = Color.cyan;
+			tw = spriteRenderer.material.DOColor(Color.yellow, delay).SetEase(Ease.Linear);
+			yield return tw.WaitForCompletion();
+			tw = spriteRenderer.material.DOColor(Color.magenta, delay).SetEase(Ease.Linear);
+			yield return tw.WaitForCompletion();
+			tw = spriteRenderer.material.DOColor(Color.cyan, delay).SetEase(Ease.Linear);
+			yield return tw.WaitForCompletion();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		defaultMaterial = spriteRenderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
