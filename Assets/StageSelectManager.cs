7a7159c0e22252110ab83bc3cd95a196131105f5
Minjung Kim <Reny.atkins@gmail.com>
2018-02-17 14:14:54 +0900

    계산서 팝업/다운

---
+++ Assets/StageSelectManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class StageSelectManager : MonoBehaviour {
+
+	public GameObject missionPanel;
+
+	public void ShowMissionPanel() {
+		Vector3 endPos = new Vector3(Screen.width/2, Screen.height/2, 0);
+		float delay = 0.5f;
+		missionPanel.GetComponent<RectTransform>().DOMove(endPos, delay);
+	}
+
+	public void HideMissonPanel() {
+		Vector3 endPos = new Vector3(Screen.width/2, -Screen.height/2, 0);
+		float delay = 0.5f;
+		missionPanel.GetComponent<RectTransform>().DOMove(endPos, delay);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
