034ebeaf994f930311641556f5877e499bae13cc
Minjung Kim <Reny.atkins@gmail.com>
2018-02-17 15:34:15 +0900

    월드 스크롤 적용

--- Assets/StageSelectManager.cs
+++ Assets/StageSelectManager.cs
@@ -7,11 +7,14 @@ using DG.Tweening;
 public class StageSelectManager : MonoBehaviour {
 
 	public GameObject missionPanel;
+	public Image missionPanelBg;
 
 	public void ShowMissionPanel(string stageName) {
 		Vector3 endPos = new Vector3(Screen.width/2, Screen.height/2, 0);
 		float delay = 0.5f;
 		missionPanel.GetComponent<RectTransform>().DOMove(endPos, delay);
+		missionPanelBg.DOFade(0.4f, delay);
+		missionPanelBg.raycastTarget = true;
 
 		missionPanel.GetComponent<MissionPanel>().LoadMissonInfo(stageName);
 	}
@@ -20,15 +23,7 @@ public class StageSelectManager : MonoBehaviour {
 		Vector3 endPos = new Vector3(Screen.width/2, -Screen.height/2, 0);
 		float delay = 0.5f;
 		missionPanel.GetComponent<RectTransform>().DOMove(endPos, delay);
-	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		missionPanelBg.DOFade(0, delay);
+		missionPanelBg.raycastTarget = false;
 	}
 }
