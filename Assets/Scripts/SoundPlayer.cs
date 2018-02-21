b0c5d55bf2575e5483c57c693a94c29e136cb989
Myeongzin Son <sonmyungzin1@naver.com>
2018-02-22 04:19:06 +0900

    사운드 파일 추가 (Background Resources는 용량문제로 일부만 추가), SoundManager 및 SoundPlayer 만듦

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour{
+    public static float soundVolume = 1;
+    public static float musicVolume = 1;
+    bool isMusicPlayer = false;
+    AudioSource audio;
+
+    public void SetMusicPlayer(){
+        isMusicPlayer = true;
+    }
+    void Awake(){
+        audio = GetComponent<AudioSource>();
+    }
+    public void Play(AudioClip clip, Vector2 pos = new Vector2(), bool loop = false){
+        audio.Stop();
+        audio.clip = clip;
+        transform.position = pos;
+        audio.Play();
+        if (isMusicPlayer) audio.loop = loop;
+        else StartCoroutine(PushAfterDelay(clip.length));
+    }  
+    IEnumerator PushAfterDelay(float length){
+        yield return new WaitForSeconds(length);
+        SoundManager.PushUsedSoundPlayer(this);
+    }
+    public void Stop(){
+        audio.Stop();
+    }
+    void Update(){
+        audio.volume = isMusicPlayer? musicVolume : soundVolume;
+    }
+}
