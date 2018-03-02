e442e71e6aac6ed57074701dafaecb2145b84f2e
youngsang <elbry92@gmail.com>
2018-03-02 12:58:35 +0900

    -버튼 관련 스크립트 통합 -튜토리얼 씬 스크린 사이즈에 맞게 변경되도록 수정

---
+++ Assets/Scripts/SceneButtonMethods.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonMethods : MonoBehaviour
+{
+    public void GoToWorld()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void GoToTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void GoIngame()
+    {
+        SceneManager.LoadScene("Ingame");
+    }
+
+    public MissionPanel missionPanel;
+
+    public void GoToStage()
+    {
+        if (missionPanel.resetTimeItem.isOn == true) MissionData.gotTimeItem = true;
+        if (missionPanel.superfoodItem.isOn == true) MissionData.gotSuperfood = true;
+        if (missionPanel.renewTrayItem.isOn == true) MissionData.gotTrayItem = true;
+        int progress = PlayerPrefs.GetInt("Progress", -1);
+        if (progress < 2)
+        {
+            GoToTutorial();
+            return;
+        }
+        GoIngame();
+    }
+
+    public GameObject[] Tutorials;
+
+    int index = 0;
+
+    public void NextTutorialPanel()
+    {
+        index++;
+        foreach (var tutorial in Tutorials)
+            tutorial.SetActive(false);
+        Tutorials[index].SetActive(true);
+        if (index > 4)
+            GoIngame();
+    }
+
+    public void PrevTutorialPanel()
+    {
+        index--;
+        foreach (var tutorial in Tutorials)
+            tutorial.SetActive(false);
+        if (index < 1)
+            GoToWorld();
+        else
+            Tutorials[index].SetActive(true);
+    }
+}
--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -5,38 +5,5 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour {
-    public GameObject[] Tutorials;
 
-    int index = 0;
-
-    public void NextTutorialPanel()
-    {
-        index++;
-        foreach (var tutorial in Tutorials)
-            tutorial.SetActive(false);
-        Tutorials[index].SetActive(true);
-        if (index > 4)
-            GoIngame();
-    }
-
-    public void PrevTutorialPanel()
-    {
-        index--;
-        foreach (var tutorial in Tutorials)
-            tutorial.SetActive(false);
-        if (index < 1)
-            GoWorld();
-        else
-            Tutorials[index].SetActive(true);
-    }
-
-    public void GoIngame()
-    {
-        SceneManager.LoadScene("Ingame");
-    }
-
-    public void GoWorld()
-    {
-        SceneManager.LoadScene("World");
-    }
 }
