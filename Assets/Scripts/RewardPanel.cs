f5f6e73968a1f2d95cdc8493db6d14ab8a91ce72
youngsang <elbry92@gmail.com>
2018-03-29 22:39:44 +0900

    아이템 획득 패널에서 어떤 아이템을 얻었는지 알려주도록 하기, 아이템 패널의 확인 버튼 판정 범위 확대

--- Assets/Scripts/RewardPanel.cs
+++ Assets/Scripts/RewardPanel.cs
@@ -30,27 +30,33 @@ public class RewardPanel : MonoBehaviour {
             yield return null;
         }
 
+        string itemString = "";
+
         switch (spriteIndex)
         {
             // 0은 타이머 리셋, 1은 만능음식, 2는 판갈기
             case 0:
                 {
                     PlayerPrefs.SetInt("TimerReset", PlayerPrefs.GetInt("TimerReset", 0) + 1);
+                    itemString = "타이머";
                 }
                 break;
             case 1:
                 {
                     PlayerPrefs.SetInt("Superfood", PlayerPrefs.GetInt("Superfood", 0) + 1);
+                    itemString = "만능음식";
                 }
                 break;
             case 2:
                 {
                     PlayerPrefs.SetInt("TrayReset", PlayerPrefs.GetInt("TrayReset", 0) + 1);
+                    itemString = "트레이";
                 }
                 break;
         }
 
         checkButton.SetActive(true);
+        checkButton.GetComponentInChildren<Text>().text = itemString + " 아이템을 얻었어요!";
     }
 
     public void SelectItem()
