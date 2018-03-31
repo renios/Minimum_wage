93944b81dd4165d1f2f4e16b154cad3fb026f527
Minjung Kim <Reny.atkins@gmail.com>
2018-03-31 21:36:48 +0900

    토끼 생성을 데이터리스트에서 받아와서 하도록 구조 변경(변경중)

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -9,32 +9,42 @@ public class Customer : MonoBehaviour {
 
 	public int indexInArray;
 
+	// newRabbit.index = 1;
+	// newRabbit.releaseStageIndex = 1;
+	// newRabbit.gender = Gender.Male;
+	// newRabbit.imageName = "yoonsung";
+	// newRabbit.waitingTime = 40;
+	// newRabbit.reduceHeartsByFail = 1;
+	// newRabbit.variableOfOrderFood = new List<int> {3, 4};
+	// 토끼 기본 스탯
+	public Rabbit rabbitData;
+
 	public float waitingTime;
 	public float remainWaitingTime;
-    public float toleranceRate;
+	public float toleranceRate;
 	public Image timerImage;
 	public Image customerImage;
-    public GameObject[] orderToBeDestroyed;   // 매칭 애니메이션 때 손님 이미지와 주문판 이미지들을 분리하기 위해 미리 주문판 이미지들을 받아놓음
+	public GameObject[] orderToBeDestroyed;   // 매칭 애니메이션 때 손님 이미지와 주문판 이미지들을 분리하기 위해 미리 주문판 이미지들을 받아놓음
 	public Vector3 customerImageOriginPos;
-    public bool isServeCompleted;            // 서빙 완료돼서 나갈때 true
+	public bool isServeCompleted;            // 서빙 완료돼서 나갈때 true
 	public bool isServed = false;            // 동시체크를 위한 변수
-	public Enums.Gender gender;				// 효과음 성별 구분용
+	public Gender gender;				// 효과음 성별 구분용
 	public int rabbitIndex;					// 이미지 중복 체크용
-    public float furyRate;
-    public float maxFuryRate;
-    public float furyCount = 0;
+	public float furyRate;
+	public float maxFuryRate;
+	public float furyCount = 0;
 
-    public List<FoodInOrder> orderedFoods = new List<FoodInOrder>();
+	public List<FoodInOrder> orderedFoods = new List<FoodInOrder>();
 
-    public bool startedFury = false;
+	public bool startedFury = false;
 	bool initialized = false;
 
 	CustomerManager customerManager;
 	GameManager gameManager;
 	GameStateManager gameStateManager;
 
-	void InitializeTimer(float inputTime) {
-		waitingTime = inputTime;
+	void InitializeTimer() {
+		waitingTime = rabbitData.waitingTime;
 		remainWaitingTime = waitingTime;
 		timerImage.color = new Color(131f / 255f, 193f / 255f, 193f / 255f, 1f);
 		timerImage.fillAmount = remainWaitingTime / waitingTime;
@@ -62,30 +72,20 @@ public class Customer : MonoBehaviour {
 		}
 	}
 
-	void SetRandomImage() {
-		//gender = (Enums.Gender)Random.Range(0,2);
-		//Debug.Log("Customer's gender : "+ gender.ToString());
-		//string spritePath = "customers/" + gender.ToString();
-		//Object[] spriteObjects = Resources.LoadAll(spritePath, typeof(Sprite));
-		//int pickedIndex = Random.Range(0, spriteObjects.Length);
-		//Sprite pickedSprite = spriteObjects[pickedIndex] as Sprite;
-		//customerImage.sprite = pickedSprite;
-		RabbitInformation.LoadSprites();
-		var selectedRabbit = RabbitInformation.SelectRabbit();
-		customerImage.sprite = selectedRabbit.sprite;
-		gender = selectedRabbit.gender;
-		rabbitIndex = selectedRabbit.index;
-		//Debug.Log("Index of Customer : "+rabbitIndex);
+	void SetImage() {
+		string spriteName = rabbitData.gender.ToString() + "/" + rabbitData.imageName;
+		Sprite customerSprite = Resources.Load<Sprite>("customers/" + spriteName);
+		customerImage.sprite = customerSprite;
 	}
 
 	// Use this for initialization
-	public void Initialize (int indexInArray, float inputTime) {
+	public void Initialize (int indexInArray, Rabbit rabbitData) {
 		this.indexInArray = indexInArray;
+		this.rabbitData = rabbitData;
 
-		SetRandomImage();
-
+		SetImage();
+		InitializeTimer();
 		MakeOrder();
-		InitializeTimer(inputTime);
 
 		initialized = true;
 	}
@@ -127,7 +127,7 @@ public class Customer : MonoBehaviour {
 		}
 
 		if (remainWaitingTime <= 0) {
-			SoundManager.PlayCustomerReaction(gender, false);
+			SoundManager.PlayCustomerReaction(rabbitData.gender, false);
 			startedFury = false;
 			furyCount = 0;
 			customerManager.RemoveCustomerByTimeout(indexInArray);
