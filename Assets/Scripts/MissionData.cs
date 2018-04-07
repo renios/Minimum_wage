9b025ef8b4c412d8ce6a991fc864720b388e1f2b
Minjung Kim <Reny.atkins@gmail.com>
2018-04-07 17:47:33 +0900

    1~3별 조건 균등하지 않아도 동작하도록 수정 (현재 1/2별 조건 지정되어 있지 않음. 지정 필요)

--- Assets/Scripts/MissionData.cs
+++ Assets/Scripts/MissionData.cs
@@ -13,6 +13,8 @@ public enum MissionDataType {
 	customerCooldown,
 	maxCustomer,
 	foodTypeCount,
+	starTrigger1,
+	starTrigger2,
 	starTrigger3
 }
 
@@ -26,6 +28,8 @@ public static class MissionData {
 	public static bool gotTimeItem = false;
 	public static bool gotSuperfood = false;
 	public static bool gotTrayItem = false;
+	public static int starTrigger1 = 200;
+	public static int starTrigger2 = 300;
 	public static int starTrigger3 = 500;
     // 아이템 보상을 주는 스테이지
     public static int[] rewardingStage = { 1, 3, 5, 7, 9,
@@ -50,6 +54,8 @@ public static class MissionData {
 		customerCooldown = 1;
 		maxCustomer = 4;
 
+		starTrigger1 = 200;
+		starTrigger2 = 300;
 		starTrigger3 = 500;
 	}
 
@@ -73,6 +79,8 @@ public static class MissionData {
 			maxHeart = missionDataDict[MissionDataType.maxHeart];
 		}
 
+		starTrigger1 = missionDataDict[MissionDataType.starTrigger1];
+		starTrigger2 = missionDataDict[MissionDataType.starTrigger2];
 		starTrigger3 = missionDataDict[MissionDataType.starTrigger3];
 
 		waitingTime = missionDataDict[MissionDataType.waitingTime];
@@ -98,6 +106,8 @@ public static class MissionData {
 			missionDataDict.Add(MissionDataType.foodTypeCount, foodTypeCount);
 		}
 
+		missionDataDict.Add(MissionDataType.starTrigger1, starTrigger1);
+		missionDataDict.Add(MissionDataType.starTrigger2, starTrigger2);
 		missionDataDict.Add(MissionDataType.starTrigger3, starTrigger3);
 
 		missionDataDict.Add(MissionDataType.maxHeart, maxHeart);
@@ -109,6 +119,8 @@ public static class MissionData {
 	}
 
 	public static Dictionary<MissionDataType, int> SetDefaultValues(Dictionary<MissionDataType, int> missionDataDict) {
+		missionDataDict.Add(MissionDataType.starTrigger1, starTrigger1);
+		missionDataDict.Add(MissionDataType.starTrigger2, starTrigger2);
 		missionDataDict.Add(MissionDataType.starTrigger3, starTrigger3);
 
 		missionDataDict.Add(MissionDataType.maxHeart, maxHeart);
@@ -136,7 +148,7 @@ public static class MissionData {
 			missionDataDict.Add(MissionDataType.customerCount, 10);
 			missionDataDict.Add(MissionDataType.foodTypeCount, 5);
 
-			missionDataDict[MissionDataType.starTrigger3] = 10*100*3/2;
+			missionDataDict[MissionDataType.starTrigger3] = 10 * 100 * 3 / 2;
 		}
 		else if (stageName == "1-2") {
 			missionDataDict.Add(MissionDataType.remainTime, 90);
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -13,15 +13,28 @@ public class ScoreManager : MonoBehaviour {
 	public Sprite inactiveBunny;
 	public Sprite activeBunny;
 
+	public Transform leftPivot;
+	public Transform rightPivot;
+
 	int visualScoreAmount = 0;
 	public int realScoreAmount = 0;
 
 	public int numberOfStars = 0;
 
-	int starTrigger3 = 0;
+	Dictionary<int, int> starTriggers;
 
 	readonly int comboCoef = 5;
 	readonly int customerCoef = 2;
+
+	void SetCheckPointPos() {
+		float deltaDist = rightPivot.position.x - leftPivot.position.x;
+		float checkPoint1PosX = leftPivot.position.x + 
+								(deltaDist * (starTriggers[1]/(float)starTriggers[3]));
+		checkPoint1.transform.position = new Vector3(checkPoint1PosX, checkPoint1.transform.position.y, checkPoint1.transform.position.z);
+		float checkPoint2PosX = leftPivot.position.x + 
+								(deltaDist * (starTriggers[2]/(float)starTriggers[3]));
+		checkPoint2.transform.position = new Vector3(checkPoint2PosX, checkPoint2.transform.position.y, checkPoint2.transform.position.z);
+	}
 
 	void ActivePoint(Image point) {
 		point.GetComponentInChildren<ParticleSystem>().Play();
@@ -42,7 +55,7 @@ public class ScoreManager : MonoBehaviour {
 
 	public void AddScoreAmountByCustomer(Customer customer) {
 		int amount = 0;
-		amount = (int)(customer.remainWaitingTime / (float)starTrigger3);
+		amount = (int)(customer.remainWaitingTime / (float)starTriggers[3]);
 		amount *= customerCoef;
 		AddScoreAmount(amount);
 	}
@@ -62,26 +75,39 @@ public class ScoreManager : MonoBehaviour {
 		visualScoreAmount = 0;
 		numberOfStars = 0;
 		bar.fillAmount = 0;
-		InactiveAllPoints();
-
+		starTriggers = new Dictionary<int, int>();
 		Dictionary<MissionDataType, int> missionDataDict = MissionData.GetMissionDataDict();
 		if (missionDataDict.ContainsKey(MissionDataType.starTrigger3)) {
-			starTrigger3 = MissionData.GetMissionDataDict()[MissionDataType.starTrigger3];
+			starTriggers[3] = MissionData.GetMissionDataDict()[MissionDataType.starTrigger3];
 		}
+		if (missionDataDict.ContainsKey(MissionDataType.starTrigger2)) {
+			starTriggers[2] = MissionData.GetMissionDataDict()[MissionDataType.starTrigger2];
+		}
+		if (missionDataDict.ContainsKey(MissionDataType.starTrigger1)) {
+			starTriggers[1] = MissionData.GetMissionDataDict()[MissionDataType.starTrigger1];
+		}
+
+		// 임시변수 예외처리
+		if (starTriggers[2] > starTriggers[3]) starTriggers[2] = starTriggers[3];
+		if (starTriggers[1] > starTriggers[2]) starTriggers[1] = starTriggers[2];
+
+		SetCheckPointPos();
+		InactiveAllPoints();
+
 	}
 
 	int amount = 1;
 
 	public void CheckStarPoint() {
-		if (visualScoreAmount > starTrigger3/3f && numberOfStars < 1) {
+		if (visualScoreAmount > starTriggers[1] && numberOfStars < 1) {
 			ActivePoint(checkPoint1);
 		}
 
-		if (visualScoreAmount > (starTrigger3*2)/3f && numberOfStars < 2) {
+		if (visualScoreAmount > starTriggers[2] && numberOfStars < 2) {
 			ActivePoint(checkPoint2);
 		}
 
-		if (visualScoreAmount >= starTrigger3 && numberOfStars < 3) {
+		if (visualScoreAmount >= starTriggers[3] && numberOfStars < 3) {
 			ActivePoint(checkPoint3);
 		}
 	}
@@ -91,7 +117,7 @@ public class ScoreManager : MonoBehaviour {
 		for (int i = 0; i < 10; i++){
 			if (visualScoreAmount < realScoreAmount) {
 				visualScoreAmount += amount;
-				bar.fillAmount = visualScoreAmount / (float)starTrigger3;
+				bar.fillAmount = visualScoreAmount / (float)starTriggers[3];
 			}
 		}
 
